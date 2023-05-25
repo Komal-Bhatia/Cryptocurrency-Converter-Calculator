1c8afbc20b5a2b17b59fd4777df5ec0ca3bb031f
Komal-Bhatia <bhatiak5475@gmail.com>
2023-05-25 20:31:41 +0530

    Add files via upload

---
+++ crypto currency calculator/script.js
@@ -0,0 +1,22 @@
+function functionToExecute() {
+    var crypval = document.getElementById("crypval").value;
+    var selectedCrypto = document.getElementById("selectedCrypto").value;
+    var selectedCurr = document.getElementById("selectedCurr").value;
+    
+    // Crypto Exchange rate in US $
+    const crypto = {'btc':26000, 'eth':1700, 'usdt':1, 'bnb': 300, 'xrp':0.45}
+    console.log("crypto: "+crypto);
+    // Currency exchange rates
+    const currency = {'usd': 1, 'aus':1.50, 'ir':80, 'brl':4.96, 'qar': 3.64}
+
+    var result = (crypval*(crypto[selectedCrypto])*(currency[selectedCurr])).toFixed(2);
+
+    document.getElementById('currval').value=result;
+    document.getElementById('currval').style.backgroundColor = '#9BA4B5';
+    document.getElementById('currval').style.color = 'white';
+
+    document.getElementById('dynamicVal').innerHTML="1 "+selectedCrypto.toUpperCase()+" equals "+(crypto[selectedCrypto])*(currency[selectedCurr])+" "+selectedCurr.toUpperCase();
+
+    document.getElementById('dynamicVal').style.color = '#9BA4B5';
+   
+}
